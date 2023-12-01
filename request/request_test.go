437cf22d332282033fd60b75dc246c8fea39035f
sknoww <hs.sknow@gmail.com>
2023-11-30 18:27:34 -0700

    Adds initial testing

--- request/request_test.go
+++ request/request_test.go
@@ -1,7 +1,34 @@
 package request
 
-import "testing"
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestParseRequest(t *testing.T) {
-	t.Log("Testing ParseRequest")
+	var newRequest InputRequest
+	newRequest.Filepath = "../test/testRequest.json"
+	parsedRequest := parseRequest(newRequest)
+
+	assert.Equal(t, "GET", parsedRequest.Method)
+	assert.Equal(t, "http://www.example.com/", parsedRequest.Url)
+	assert.Equal(t, "application/json", parsedRequest.Headers["Content-Type"])
+}
+
+// TODO: Test executeHttpRequest, need to mock http response
+
+func TestParseResponse(t *testing.T) {
+	response := &http.Response{
+		StatusCode: 200,
+		Header:     http.Header{"Content-Type": []string{"application/json"}},
+		Body:       io.NopCloser(bytes.NewBufferString(`{"foo":"bar"}`)),
+	}
+	parsedResponse := parseResponse(response)
+
+	assert.Equal(t, 200, parsedResponse.StatusCode)
+	assert.Equal(t, "application/json", parsedResponse.ContentType)
 }
