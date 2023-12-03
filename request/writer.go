75669eb40fd6f00d08c15210a70d281faa9d1b65
sknoww <hs.sknow@gmail.com>
2023-12-02 23:27:34 -0700

    Adds initial writing for html and text/plain

--- request/writer.go
+++ request/writer.go
@@ -8,6 +8,12 @@ import (
 
 // OutputResponseToStd writes the response body to stdout and stderr (default)
 func OutputResponseToStd(response *HttpResponse) {
+	// If content type is not json write response body to stdout
+	if response.Headers["Content-Type"] != "application/json" {
+		fmt.Printf("%s\n", response.Body)
+		return
+	}
+	// If content type is json, write response body to stdout in json format
 	// Write response body to stdout in json format
 	b := marshalResponse(response.Body)
 	fmt.Printf("%s\n", string(b))
